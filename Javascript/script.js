625d22fcfab17cead2bfff886396da2e02d8636c
AlbertoScalia <albscalia@gmail.com>
2023-04-14 20:42:59 +0200

    Feat (Javascript) : Add Javascript

---
+++ Javascript/script.js
@@ -0,0 +1,7 @@
+const priceForKilometer = 0.21;
+let nameOfPassenger = document.getElementById('name');
+let numberOfKilometres = document.getElementById('km');
+let ageOfPassenger = document.getElementById('select');
+let submit = document.getElementById('generate');
+
+
