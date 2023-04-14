c0d51a4e415d0e7913cdb71783fc7f4fd6d16c61
AlbertoScalia <albscalia@gmail.com>
2023-04-14 20:50:26 +0200

    Feat (Index & Javascript) : Add addEventListener

--- Javascript/script.js
+++ Javascript/script.js
@@ -5,3 +5,27 @@ let ageOfPassenger = document.getElementById('select');
 let submit = document.getElementById('generate');
 
 
+
+submit.addEventListener('click', function() {
+    const passengerName = nameOfPassenger.value;
+    const numKm = numberOfKilometres.value;
+    let passengerAge = ageOfPassenger.value;
+    let price = numKm * priceForKilometer;
+
+    if (passengerAge === 'under_18') {
+        const discount20 = price * 20 / 100;
+        price -= discount20;
+
+    } else if (passengerAge === 'over_65') {
+        const discount40 = price * 40 / 100;
+        price -= discount40;
+    }
+
+    let carrozzaAssegnata = Math.ceil(Math.random() * 12);
+    console.log("carrozza", carrozzaAssegnata);
+
+    let cpCode = Math.floor(Math.random() * 100000) + 1;
+})
+
+
+
