f24fa6575883b9b7682417eb129af14786c2c455
AlbertoScalia <albscalia@gmail.com>
2023-04-14 20:56:57 +0200

    Feat (Javascript) : Add innerHTML and reset

--- Javascript/script.js
+++ Javascript/script.js
@@ -4,9 +4,7 @@ let numberOfKilometres = document.getElementById('km');
 let ageOfPassenger = document.getElementById('select');
 let submit = document.getElementById('generate');
 
-
-
-submit.addEventListener('click', function() {
+submit.addEventListener('click', function () {
     const passengerName = nameOfPassenger.value;
     const numKm = numberOfKilometres.value;
     let passengerAge = ageOfPassenger.value;
@@ -25,6 +23,43 @@ submit.addEventListener('click', function() {
     console.log("carrozza", carrozzaAssegnata);
 
     let cpCode = Math.floor(Math.random() * 100000) + 1;
+    const card = document.getElementById('card_container');
+    card.innerHTML =
+        `<h4 class="mb-0 text-uppercase fw-bold text-center text-white">Il tuo biglietto</h4>
+    <div class="card mt-3">
+    <div class="card-header d-flex align-items-center">
+    <h5 class="mb-0 ps-1 text-uppercase">Dettaglio passeggeri</h5>
+    </div>
+    <div class="card-body">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col" class="text-uppercase" >Nome passeggero</th>
+                    <th scope="col">Offerta</th>
+                    <th class="collapse d-md-table-cell" scope="col">Carrozza</th>
+                    <th class="collapse d-md-table-cell" scope="col">Codice CP</th>
+                    <th scope="col">Costo biglietto</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row" class="fw-bold">${passengerName}</th>
+                    <td>Biglietto standard</td>
+                    <td class="collapse d-md-table-cell">${carrozzaAssegnata}</td>
+                    <td class="collapse d-md-table-cell">${cpCode}</td>
+                    <td>€ ${price.toFixed(2)}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `;
+})
+
+const reset = document.getElementById('reset');
+
+reset.addEventListener('click', function () {
+    nameOfPassenger.value = "";
+    numberOfKilometres.value = "";
 })
 
 
